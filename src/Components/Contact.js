72a1f3e3050a63e4056531324dba9acc736b653e
Diwakar Tiwari <tiwarydiwakar1@gmail.com>
2022-11-11 14:14:03 +0530

    Site title update

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -16,7 +16,7 @@ function Contact() {
           <div>
             <li>
               <i className='fa fa-map-marker'></i>
-              <p>138, Darpan Green, Kharar,Mohalli, Punjab India</p>
+              <p>Chandigarh University, Kharar,Mohalli, Punjab India</p>
             </li>
             <li>
               <i className='far fa-envelope'></i>
@@ -32,8 +32,19 @@ function Contact() {
             </li>
           </div>
 
+          <div className='contactForm'>
+          <label>Name:<input type="text" name="client-name" required/></label>
+        <br/>
+        <label>Email:<input type="text" name="client-email" required/></label>
+        <br/>
+        <label>Comment:<br/><textarea name="comment"></textarea></label>
+        <br/>
+        <input type="submit" value="Submit"/>
+        </div>
+
+
           <div className="map">
-          {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3428.205166210261!2d76.57242631497647!3d30.76882068162447!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ffb140bd63e07%3A0x68591e334d17a988!2sChandigarh%20University!5e0!3m2!1sen!2sin!4v1663847438314!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
+            {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3428.205166210261!2d76.57242631497647!3d30.76882068162447!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ffb140bd63e07%3A0x68591e334d17a988!2sChandigarh%20University!5e0!3m2!1sen!2sin!4v1663847438314!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
           </div>
         </div>
       </section>
